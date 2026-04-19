831b5785e675bbc6d1f63b031eca3a5f0452bf80
sNj <Sh1nJiTEITA.TWSOL@gmail.com>
2026-04-19 11:39:10 +0000

    Fix for new arch

--- startup.zsh
+++ startup.zsh
@@ -11,13 +11,14 @@ source $ZSH/oh-my-zsh.sh
 
 # oh-my-posh ==============================
 # Adding oh-my-posh binaries to path
-export PATH=$PATH:/home/snj/.local/bin
+export PATH=$PATH:"$HOME/.local/bin"
+export PATH=$PATH:"$HOME/.oh-my-posh"
 
 # Loading oh-my-posh theme
 # eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/tiwahu.omp.json)"
 # eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/hul10.omp.json)"
 # eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/clean-detailed.omp.json)"
-eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/snj.omp.yaml)"
+eval "$(oh-my-posh init zsh --config ~/.config/omp/snj.omp.yaml)"
 
 
 # ZOXIDE ==============================
